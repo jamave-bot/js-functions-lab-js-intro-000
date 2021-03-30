cdb2c88eb9ec688693287e0edd282c6a303390e0
Jason V <jamave777@gmail.com>
2021-03-30 22:00:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} until ${holiday}!`
+}
